363354140718d57e8632ba2d0973519586747456
Juan <juanito.lansangan@gmail.com>
2025-02-01 03:15:01 +0800

    fix again

--- ci-deploy/scripts.sh
+++ ci-deploy/scripts.sh
@@ -9,7 +9,7 @@ cd /root/ci-deploy
 echo "***********************************"
 echo "Stop the application"
 echo "***********************************"
-docker compose down
+docker-compose down
 
 echo "***********************************"
 echo "Removing all docker images"
@@ -19,7 +19,7 @@ docker image prune -a -f
 echo "***********************************"
 echo "Start the application"
 echo "***********************************"
-docker compose up -d
+docker-compose up -d
 
 echo "***********************************"
 echo "Application is now running!"
